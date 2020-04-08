17afe8567dde7ac4262eec6f68e7bbfb0dd830c0
choisewon <chltpdnjs5@naver.com>
2020-04-08 12:13:31 +0900

    RobotWalk(); 안의 내부를 작성하였습니다.

--- Robot.java
+++ Robot.java
@@ -7,8 +7,25 @@
  */
 public class Robot
 {
-    public Robot()
+    public void RobotWalk()
     {
-        
+        double speed = 10;
+        String tratfic_signal;
+        if (tratfic_signal == "녹색")
+        {
+            
+        }
+        else if (tratfic_signal == "노란색")
+        {
+            speed = speed*1.2;
+        }
+        else
+        {
+            speed = 0;
+        }
+    }
+    public static void main(String[] args)
+    {
+        RobotWalk();
     }
 }
